329eca14d804628d4f4607861a64d9d04c447459
Cory G Watson <gphat@cpan.org>
2008-03-21 13:25:33 +0000

    Import of GPHAT/Osgood-Server-1.1.0 from CPAN.
    gitpan-cpan-distribution: Osgood-Server
    gitpan-cpan-version:      1.1.0
    gitpan-cpan-path:         GPHAT/Osgood-Server-1.1.0.tar.gz
    gitpan-cpan-author:       GPHAT
    gitpan-cpan-maturity:     released

--- lib/Osgood/Server.pm
+++ lib/Osgood/Server.pm
@@ -15,7 +15,7 @@ use Catalyst::Runtime '5.70';
 
 use Catalyst qw/-Debug ConfigLoader Static::Simple/;
 
-our $VERSION = '1.0.6';
+our $VERSION = '1.1.0';
 our $AUTHORITY = 'cpan:GPHAT';
 
 # Configure the application. 
@@ -35,25 +35,107 @@ __PACKAGE__->setup;
 
 =head1 NAME
 
-Osgood::Server - Catalyst based application
+Osgood::Server - Event Repository
 
 =head1 SYNOPSIS
 
+    create a database (mysql in our example)
+    mysql -u root your_database < sql/schema.sql
+    edit osgood_server.yml
     script/osgood_server_server.pl
 
 =head1 DESCRIPTION
 
-[enter your description here]
+Osgood is a passive, persistent, stateless event repository.
+
+* Passive: Osgood doesn't seek out events, it only waits for notification of
+them.
+
+* Persistent: Events are durable, they do not disappear if the server goes
+offline.
+
+* Stateless: Events in Osgood are read-only.  You cannot change them.
+
+What's all that fancy talk mean?  Osgood is a system wherein you record the
+fact that something happened.  A client library (L<Osgood::Client> that allows
+you to send events to Osgood and to later retrieve them.
+
+Effectively we are talking about an implementation of the Publisher /
+Subscriber model.  This implementation is built using HTTP as the medium
+and a relational database as the backing store.
+
+An example may help to illustrate the usefulness.
+
+Imagine a company that sells widgets.  Sometimes, customers are unhappy and
+cancel the widgets.  When a customer calls to do so, the call center dutifully
+cancels the order.  This works fine and the world is a happy place.
+
+One day a new marketing employee decides to try and woo these customers back.
+She asks you to build a system to send an email to a customer that cancels,
+promising a $5 coupon to come back for more widgets!  This is a great idea,
+and it is a great chance to use Osgood.
+
+When the cancellation happens, you add a smidge of code that uses
+Osgood::Client to send an event.  You might use descriptions like:
+
+  my $event = new Osgood::Event(
+      object    => 'Order',
+      action    => 'canceled',
+      params    => {
+          id    => 12345
+      }
+  );
+  my $list = new Osgood::EventList(events => [ $event ]);
+  $client->send($list);
+
+Meaning that Order #12345 was canceled.
+
+Later, you whip up a job that runs every hour that queries Osgood for all
+the orders that have been canceled.
+
+  $client->query({
+      object    => 'Order',
+      action    => 'canceled',
+  });
+
+Now you have a list!  But every call returns ALL the Order canceled events that
+happened, ever!  The query method provides a bunch of constraints to fix that.
+The most useful of which is 'id'.  Providing query with an id limits the
+returned events to all the events that have an ID GREATER THAN THE ONE
+PROVIDED.
+
+There are other constraints provided in L<Osgood::Server::Controller::Event>
+such as date_before and date_after.
+
+The advantage of this implementation is that you can create subscribers
+to an event without adding any new code to your cancellation process.  State
+is maintained by the jobs that do the querying, however you choose to
+implement it.  We use ids, you might use dates.
+
+IMPORTANT: 
+
+=head1 PERFORMANCE
+
+Note: See the accompanying section of Osgood::Client as well.
+
+Osgood uses some DBIx::Class shortcuts to pull results faster.  Depending on
+database hardware, small numbers of events (hundreds) should be really fast.
+Tests have been conducted on lists of 10_000 events and the response time still
+falls within ::Client's default 30 second timeout on modern hardware.
 
 =head1 SEE ALSO
 
 L<Osgood::Server::Controller::Root>, L<Catalyst>, L<Osgood::Client>
 
-=head1 AUTHOR
+=head1 AUTHORS
 
 Lauren O'Meara
 
 Cory 'G' Watson <gphat@cpan.org>
+
+=head1 CONTRIBUTORS
+
+Guillermo Roditi (groditi)
 
 =head1 COPYRIGHT AND LICENSE
 
